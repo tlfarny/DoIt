0c2542b578ec36ba178a69a8251d20d4b0706443
Travis Farnsworth <travis.l.farnsworth@gmail.com>
2017-04-07 07:39:54 -0600

    Added plusTask view controller

---
+++ DoIt/plusTaskViewController.swift
@@ -0,0 +1,25 @@
+//
+//  plusTaskViewController.swift
+//  DoIt
+//
+//  Created by Travis Farnsworth on 4/7/17.
+//  Copyright © 2017 Travis Farnsworth. All rights reserved.
+//
+
+import UIKit
+
+class plusTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
