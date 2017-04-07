dd523560e145d65b23bfa5bcda09a4248ec93078
Travis Farnsworth <travis.l.farnsworth@gmail.com>
2017-04-07 05:09:29 -0600

    task class created

---
+++ DoIt/Task.swift
@@ -0,0 +1,12 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Travis Farnsworth on 4/7/17.
+//  Copyright © 2017 Travis Farnsworth. All rights reserved.
+//
+
+class Task {
+    var name = ""
+    var important = false
+}
