3efc4142014fc3c4c3359f9380a4cdcec0cbd7be
Travis Farnsworth <travis.l.farnsworth@gmail.com>
2017-04-07 05:41:30 -0600

    populating list based off task array

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -9,24 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    var tasks : [Task] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tasks = makeTasks()
+        
         tableView.dataSource = self
         tableView.delegate = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "hello"
+        let task = tasks[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = "❗️ \(task.name)"
+        } else {
+            cell.textLabel?.text = task.name
+        }
         return cell
+    }
+    
+    func makeTasks() -> [Task] {
+        let task1 = Task(name: "Walk the dog", important: false)
+        let task2 = Task(name: "Buy cheese", important: true)
+        let task3 = Task(name: "Mow the lawn", important: false)
+        return [task1,task2, task3]
     }
 }
 
